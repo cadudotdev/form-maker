89f9b52cb884fa5dc618d9c3660cff48dd14efd4
carlos.santos <carlos.santos@neomind.com.br>
2025-07-20 00:05:11 -0300

    sprint init

---
+++ src/main/java/com/cadudotdev/form_maker/FormMakerApplication.java
@@ -0,0 +1,13 @@
+package com.cadudotdev.form_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormMakerApplication.class, args);
+	}
+
+}
